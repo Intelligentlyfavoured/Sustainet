3c500c3fe2898386f4ead6485d22b9f52a404a28
Hope <113907178+Intelligentlyfavoured@users.noreply.github.com>
2025-02-07 15:34:14 +0300

    Create Voucher

--- Sustainet/src/App.jsx
+++ Sustainet/src/App.jsx
@@ -7,38 +7,90 @@ import AuthorizerHome from "./Modules/User Pages/AuthorizerHome";
 import ReviewerHome from "./Modules/User Pages/ReviewerHome";
 import PaymentInitiatorHome from "./Modules/User Pages/PaymentInitiatorHome";
 import FinalPaymentAuthorizerHome from "./Modules/User Pages/FinalPaymentAuthorizerHome";
+import Sidebar from "./components/Sidebar";
+import CreateVoucher from "./components/create-voucher";
+// import ReviewVoucher from "./Modules/Vouchers/ReviewVoucher";
+// import AuthorizeVoucher from "./Modules/Vouchers/AuthorizeVoucher";
+// import InitiateVoucher from "./Modules/Vouchers/InitiateVoucher";
+// import PaymentVoucher from "./Modules/Vouchers/PaymentVoucher";
+// import DocumentationVoucher from "./Modules/Vouchers/DocumentationVoucher";
+// import Users from "./Modules/Users";
 
-
+const DashboardLayout = () => {
+  return (
+    <div className="dashboard-container">
+      <Sidebar />
+      <div className="dashboard-content">
+        <Outlet /> {/* This will render the selected route inside this layout */}
+      </div>
+    </div>
+  );
+};
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
+      <div className="app-container">
+        {/* Sidebar displayed for authenticated users */}
+        <Routes>
+          <Route path="/" element={<Login />} />
 
-        <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
-          <Route path="/AdminHome" element={<AdminHome />} />
-        </Route>
+          {/* Protected Routes */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path="/AdminHome" element={<AdminHome />} />
+          </Route>
 
-        <Route element={<PrivateRoute allowedRoles={["initiator"]} />}>
-          <Route path="/InitiatorHome" element={<InitiatorHome />} />
-        </Route>
+          <Route element={<PrivateRoute allowedRoles={["initiator"]} />}>
+            <Route path="/InitiatorHome" element={<InitiatorHome />} />
+          </Route>
 
-        <Route element={<PrivateRoute allowedRoles={["authorizer"]} />}>
-          <Route path="/AuthorizerHome" element={<AuthorizerHome />} />
-        </Route>
+          <Route element={<PrivateRoute allowedRoles={["authorizer"]} />}>
+            <Route path="/AuthorizerHome" element={<AuthorizerHome />} />
+          </Route>
 
-        <Route element={<PrivateRoute allowedRoles={["reviewer"]} />}>
-          <Route path="/reviewer-home" element={<ReviewerHome />} />
-        </Route>
+          <Route element={<PrivateRoute allowedRoles={["reviewer"]} />}>
+            <Route path="/reviewer-home" element={<ReviewerHome />} />
+          </Route>
 
-        <Route element={<PrivateRoute allowedRoles={["payment_initiator"]} />}>
-          <Route path="/payment-initiator-home" element={<PaymentInitiatorHome />} />
-        </Route>
+          <Route element={<PrivateRoute allowedRoles={["payment_initiator"]} />}>
+            <Route path="/payment-initiator-home" element={<PaymentInitiatorHome />} />
+          </Route>
 
-        <Route element={<PrivateRoute allowedRoles={["final_payment_authorizer"]} />}>
-          <Route path="/final-payment-authorizer-home" element={<FinalPaymentAuthorizerHome />} />
-        </Route>
-      </Routes>
+          <Route element={<PrivateRoute allowedRoles={["final_payment_authorizer"]} />}>
+            <Route path="/final-payment-authorizer-home" element={<FinalPaymentAuthorizerHome />} />
+          </Route>
+
+          <Route element={<PrivateRoute allowedRoles={["admin", "initiator"]} />}>
+            <Route path="/create-voucher" element={<CreateVoucher />} />
+          </Route>
+{/* 
+          <Route element={<PrivateRoute allowedRoles={["reviewer"]} />}>
+            <Route path="/review-voucher" element={<ReviewVoucher />} />
+          </Route>
+
+          <Route element={<PrivateRoute allowedRoles={["authorizer"]} />}>
+            <Route path="/authorize-voucher" element={<AuthorizeVoucher />} />
+          </Route>
+
+          <Route element={<PrivateRoute allowedRoles={["initiator"]} />}>
+            <Route path="/initiate-voucher" element={<InitiateVoucher />} />
+          </Route>
+
+          <Route element={<PrivateRoute allowedRoles={["payment_initiator"]} />}>
+            <Route path="/payment-voucher" element={<PaymentVoucher />} />
+          </Route>
+
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path="/documentation-voucher" element={<DocumentationVoucher />} />
+          </Route>
+
+
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path="/users" element={<Users />} />
+          </Route> */}
+
+
+        </Routes>
+      </div>
     </Router>
   );
 }
