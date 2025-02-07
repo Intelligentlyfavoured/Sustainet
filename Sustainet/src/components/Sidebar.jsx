0c66eb7c54a4d38394ce6d3464b038c15a0a3216
Benjamkapa <mkapabenjamin254@gmail.com>
2025-02-07 15:22:41 +0300

    my local commit

--- Sustainet/src/components/Sidebar.jsx
+++ Sustainet/src/components/Sidebar.jsx
@@ -1,78 +1,46 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  const [openMenus, setOpenMenus] = useState({}); // Track which menus are open
-
-  // 7 Main Options and Sub-options
+  // Sidebar Menu Options
   const menuItems = [
-  
-    { title: "Initiate voucher "},
-    { title: "Review voucher "},
-    { title: "Authorize voucher "},
-    { title: "Initiate Payment"},
-    { title: "Final Payment voucher"},
-    { title: "Documentation Voucher"},
-    
+    { title: "Initiate Voucher", path: "/initiate-voucher" },
+    { title: "Review Voucher", path: "/review-voucher" },
+    { title: "Authorize Voucher", path: "/authorize-voucher" },
+    { title: "Initiate Payment", path: "/initiate-payment" },
+    { title: "Payment Voucher", path: "/final-payment-voucher" },
+    { title: "Documentation", path: "/documentation-voucher" },
   ];
-
-  // Toggle submenu visibility
-  const toggleMenu = (index) => {
-    setOpenMenus((prev) => ({
-      ...prev,
-      [index]: !prev[index],
-    }));
-  };
 
   return (
     <div className="sidebar">
       <h3 className="sidebar-title">Dashboard</h3>
 
       <ul className="menu">
-        {/* Render 7 Main Options with Sub-options */}
+        {/* Main Navigation Links */}
         {menuItems.map((item, index) => (
           <li key={index}>
-            <button className="submenu-toggle" onClick={() => toggleMenu(index)}>
+            <Link to={item.path} className="sidebar-link">
               {item.title}
-            </button>
-            {openMenus[index] && (
-              <ul className="submenu">
-                {item.subOptions.map((sub, subIndex) => (
-                  <li key={subIndex}>
-                    <Link to={`/option${index + 1}-sub${subIndex + 1}`}>
-                      {sub}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            )}
+            </Link>
           </li>
         ))}
 
-        {/* Separator for User Groups & Users */}
+        {/* Separator */}
         <hr className="menu-separator" />
 
         {/* User Groups */}
-        <li>
-          <button className="submenu-toggle" onClick={() => toggleMenu("userGroups")}>
-            User Groups
-          </button>
-          {openMenus["userGroups"] && (
-            <ul className="submenu">
-              <li><Link to="/admin">Admin</Link></li>
-              <li><Link to="/initiator">Initiator</Link></li>
-              <li><Link to="/authorizer">Authorizer</Link></li>
-              <li><Link to="/reviewer">Reviewer</Link></li>
-              <li><Link to="/payment-initiator">Payment Initiator</Link></li>
-              <li><Link to="/final-payment-authorizer">Final Payment Authorizer</Link></li>
-            </ul>
-          )}
-        </li>
-
-        {/* Users */}
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
+        {/* <li>
+          <h4 className="menu-header">User Groups</h4>
+          <ul className="submenu">
+            <li><Link to="/admin" className="sidebar-link">Admin</Link></li>
+            <li><Link to="/initiator" className="sidebar-link">Initiator</Link></li>
+            <li><Link to="/authorizer" className="sidebar-link">Authorizer</Link></li>
+            <li><Link to="/reviewer" className="sidebar-link">Reviewer</Link></li>
+            <li><Link to="/payment-initiator" className="sidebar-link">Payment Initiator</Link></li>
+            <li><Link to="/final-payment-authorizer" className="sidebar-link">Final Payment Authorizer</Link></li>
+          </ul>
+        </li> */}
       </ul>
     </div>
   );
